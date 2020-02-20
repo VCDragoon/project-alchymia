26449852adb5d5eab0581a69e0290b8e0b3e0de2
Brandon Harris <Brandon.Harris1@ibm.com>
2020-02-20 11:40:04 -0500

    added helper function to highlight changes in pandas df

--- utilities/helpers.py
+++ utilities/helpers.py
@@ -1,6 +1,7 @@
 #TODO change over to class structure instead
 #TODO add not just for map, but for all kinds of stuff (currency, cards, etc.)
 import yaml
+import pandas as pd
 
 def map_offers_details(offer_details):
     contact_ign = offer_details["listing"]["account"]["lastCharacterName"]
@@ -27,3 +28,9 @@ def load_config():
         except yaml.YAMLError as exc:
             print(exc)
 
+
+def highlight_diff(data, color='yellow'):
+    attr = 'background-color: {}'.format(color)
+    other = data.xs('First', axis='columns', level=-1)
+    return pd.DataFrame(np.where(data.ne(other, level=0), attr, ''),
+                        index=data.index, columns=data.columns)
